73505b3b172e52eb3aef536eb84a93ab045ac311
mumia1555 <35424219+mumia1555@users.noreply.github.com>
2023-02-24 12:27:00 +0200

    Add files via upload

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        [HttpGet]
+        public ActionResult Index()
+        {
+            BlogDataEntity entity = new BlogDataEntity();
+            return View(from BlogData in entity.blogs select BlogData);
+        }
+
+        [HttpGet]
+        public ActionResult Detail(int id)
+        {
+            BlogDataEntity entity = new BlogDataEntity();
+            var blog = (from BlogData in entity.blogs.Where(b => b.id == id) select BlogData).FirstOrDefault();
+            return View(blog);
+        }
+    }
+}
